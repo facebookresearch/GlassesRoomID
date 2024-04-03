fcd66fda88400e8e24f253672e9707234b18ddb1
Thomas Deppisch <thomas.deppisch@posteo.de>
2024-04-03 15:46:54 +0200

    fix default argument in getDrr

--- lib/getDrr.m
+++ lib/getDrr.m
@@ -1,12 +1,12 @@
 % Copyright (c) Facebook, Inc. and its affiliates.
 
 function drrDb = getDrr(ir,fs,tSplitMs,freqRangeHz)
-% simple DRR calculation
-% Find direct sound peak, calculate DRR as energy ratio in a 5 ms window
-% around the peak and the rest.
-% The direct sound peak here is found as the first of the largest 3 peaks within the first 20 ms.
-% Use tSplitMs to split direct and reverberant part at some later time
-% instant.
+% Simple DRR calculation.
+% Find direct sound peak, calculate DRR as energy ratio of the direct sound
+% peak (window from 1 ms before the peak to tSplitMs after the peak) and the
+% rest.
+% The direct sound peak here is found as the first of the largest N peaks 
+% within the first 20 ms.
 
 % Thomas Deppisch, 2023
 
@@ -21,7 +21,7 @@ dirSoundWinLenSmp = dirSoundWinLenMs/1000 * fs;
 halfDirSoundWinLenSmp = round(dirSoundWinLenSmp/2);
 
 if nargin < 3 || isempty(tSplitMs)
-    tSplitMs = halfDirSoundWinLenSmp;
+    tSplitMs = halfDirSoundWinLenSmp/fs*1000;
 end
 
 [bBp,aBp] = butter(4, freqRangeHz/fs*2); % bandpass filter
